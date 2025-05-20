7f8a687adbfd531e82fb9c12c74790d8ceba6157
ac147923 <ac147923@8CG8163L09>
2025-05-20 14:15:01 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _12TPIPROJECT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
