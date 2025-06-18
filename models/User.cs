f93afc9c1c08638d847b72201af4430dcc9d7186
ac147923 <ac147923@avcol.school.nz>
2025-06-19 11:03:39 +1200

    I finished adding user model

---
+++ models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12TPIPROJECT.models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Pin { get; set; }
+        public string Role { get; set; }
+
+        public User(int userID, string username, string pin, string role)
+        {
+            UserID = userID;
+            Username = username;
+            Pin = pin;
+            Role = role;
+        }
+    }
+}
